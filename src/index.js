37ddb952a93df9b7ae1137522369fd17c37bdcb0
Anom <49372345+eko733@users.noreply.github.com>
2021-08-18 22:15:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ client.setInterval(async () => {
 
 
   client.user.setActivity(
-    `$${numberWithCommas(price)}/ ${numberWithCommas(circSupply)}%`,
+    `$${numberWithCommas(price)}/(${numberWithCommas(circSupply)}%)`,
     { type: 'WATCHING' },
   )
 }, 1*60*60)
