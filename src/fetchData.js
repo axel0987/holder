670f0068b25d832f4f9a5db3420df7e3b77159f9
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-26 04:14:22 +0700

    Update fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -7,9 +7,9 @@ exports.fetchData = async () => {
   try {
     const tokenData = await (await fetch('https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3', {
   headers: {Authentication: 'Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4'}
-})
-   .then(resp => resp.json())
-   .then( json => console.log(json))
+
+
+
 
 
 
