656d91573ace7cbc8164f5918b84c1047760d246
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-26 05:13:28 +0700

    Update fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,30 +2,19 @@ const dotenv = require('dotenv')
 const fetch = require('node-fetch')
 
 dotenv.config()
-
+const nameapi = fetch("https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3", {
+  headers: {
+    Accept: "application/json",
+    Authorization: "Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4"
+  }
+})
 exports.fetchData = async () => {
   try {
-    const token = await (await fetch(`https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3`)).json()
-    const http = {
-      headers : (
-        Authentication = 'Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4')
-      }
-      const {token, http} = tokendata
+    const tokenData = await (await fetch(`nameapi`)).json()
 
-
-
-
-
-
-
-
-
-
-
-
-    const price = tokenData.ticker
-    const symbol = tokenData.tickers
-    const circSupply = tokenData.holders
+    const price = tokenData.tickers
+    const symbol = tokenData.price
+    const circSupply = tokenData.supply
 
     return { price, symbol, circSupply }
   } catch (err) {
