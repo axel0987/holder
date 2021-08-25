5829ab3559de6d3c576cefddcfd77ba6e436a3b4
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-26 04:56:23 +0700

    Update fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -6,7 +6,10 @@ dotenv.config()
 exports.fetchData = async () => {
   try {
     const tokenData = await (await fetch(`https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3`)).json()
-    Authentication = (`Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4`)
+    const http = {
+      headers : {
+        Authentication = 'Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4'}
+  
     .then(function (response) {
     console.log(response.status);
     return response.json();
