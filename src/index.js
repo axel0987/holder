f4aef4a3396479adabd94e47d07d77494d59668a
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-26 19:32:04 +0000

    Create index.js

--- src/index.js
+++ src/index.js
@@ -13,17 +13,19 @@ client.on('ready', () => console.log(`Bot successfully started as ${client.user.
 
 // Updates token price on bot's nickname every X amount of time
 client.setInterval(async () => {
-  const data = await (await fetch("https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3", {
-  headers: {
-    Accept: "application/json",
-    Authorization: "Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4"
+  const request = require('request');
+let options = {
+  'method': 'GET',
+  'url': 'https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3',
+  'headers': {
+    'accept': 'application/json',
+    'Authorization': 'Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4'
   }
-})
-
-
-
-  const { tickers,holders,supply } = data
-
+};
+request(options, (error, response) => {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
   client.guilds.cache.forEach(async (guild) => {
     const botMember = guild.me
     await botMember.setNickname(`$${tickers}`)
