9d209071c93b585b989b60c33c665fab85165fd0
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-26 19:03:10 +0000

    Create fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -5,20 +5,16 @@ dotenv.config()
 
 exports.fetchData = async () => {
   try {
-    const token = await (
-let options = {
-  'method': 'GET',
-  'url': 'https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3',
-  'headers': {
-    'accept': 'application/json',
-    'Authorization': 'Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4'
-  }
-});
-const tokenadd = token
-    
-    const price = tokenadd.ticker
-    const symbol = tokenadd.holders
-    const circSupply = tokenadd.supply
+    const tokenData = await (await fetch("https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3", {
+  headers: {
+    Accept: "application/json",
+    Authorization: "Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4")).json()
+
+
+
+    const price = tokenData.holder
+    const symbol = tokenData.symbol
+    const circSupply = tokenData.holders
 
     return { price, symbol, circSupply }
   } catch (err) {
