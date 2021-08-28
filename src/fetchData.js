a87c2e0409337411fe8f5f82bf053437185624b0
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-28 20:40:14 +0000

    Create fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -12,6 +12,7 @@ let url = `https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVC
 exports.fetchData = async () => {
   try {
     let tokenData = await fetch(url, settings)
+    let tokenData = await res.json()
 
 
     const price = tokenData.ticker
