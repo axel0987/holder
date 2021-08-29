44090513a4b7641a04ea174e8ebc0d7a7a77551e
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-29 04:22:30 +0000

    Create index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ client.setInterval(async () => {
 
   if (!data) return
 
-  const { price, symbol, circSupply } = data
+  const { price, symbol, circSupply, duaempat, } = data
 
   client.guilds.cache.forEach(async (guild) => {
     const botMember = guild.me
