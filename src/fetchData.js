f535778c4dee97add8c2a5e0eac7e293beb0ac11
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-29 02:25:15 +0000

    Create fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,20 +2,21 @@ const dotenv = require('dotenv')
 const fetch = require('node-fetch')
 
 dotenv.config()
-
-exports.fetchData = async () => {
-  try {
-
-   let requestOptions = {
+let requestOptions = {
   method: 'GET',
   headers: 'application/json',
   Authorization: 'Bearer 70d4ef96-0cfc-4e8d-be5a-6e15569b40b4'
-};
+  }
+exports.fetchData = async () => {
+  try {
 
-const tokenData = await (await fetch("https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+
+
+
+const tokenData = await (await fetch("https://api.solanabeach.io/v1/token/BLwTnYKqf7u4qjgZrrsKeNs2EzWkMLqVCu6j8iHyrNA3", requestOptions).json()
+
+
+
 
     
 
