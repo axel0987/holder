920b9e7f414316ee9d77ef85eeffa72c4a7ef742
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-08-29 06:13:43 +0000

    Create fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -18,7 +18,7 @@ const res = await (await fetch(url, options)).json()
 
                    const prices = res.meta.marketPrice
     const symbol = res.ticker
-    const circSupply = res.holder
+    const circSupply = res.holders
     const duaempat = res.supply
 
     return { prices, symbol, circSupply, duaempat }
