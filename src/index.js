bc87f589a8b005da7f18c12968d29a3d62bac4c6
axel0987 <89490876+axel0987@users.noreply.github.com>
2021-09-11 13:04:01 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ client.setInterval(async () => {
 
   client.guilds.cache.forEach(async (guild) => {
     const botMember = guild.me
-    await botMember.setNickname(`${Number(circSupply)} Holder`)
+    await botMember.setNickname(`${Number(circSupply)} Holders`)
   })
 
 
